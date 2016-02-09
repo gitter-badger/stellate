da214140b8bd88686516fba170efa9b1353fd79c
jona <jona@jona.io>
2016-02-09 21:59:32 +0100

    feat: add about page

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,10 +1,8 @@
 class StaticController < ApplicationController
   def index
   end
-
   def faq
   end
-  
   def about
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  # Routes for static pages:
   root 'static#index'
   get 'faq' => 'static#faq'
   get 'about' => 'static#about'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
