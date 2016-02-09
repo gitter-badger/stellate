f56a90178e8e11c811d27a5a8ee0beeaba64a1b8
pixeldesu <git@pixelde.su>
2016-02-09 23:23:21 +0100

    feat: allow to login with username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
 
   has_one :profile, dependent: :destroy
 
@@ -13,5 +14,22 @@ class User < ActiveRecord::Base
 
   after_create do
     Profile.create(user_id: self.id)
+  end
+
+  def login=(login)
+    @login = login
+  end
+
+  def login
+    @login || self.screen_name || self.email
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(screen_name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
   end
 end
