feb528403473facc33cb9a72218ef61afa43698e
pixeldesu <git@pixelde.su>
2016-02-12 19:58:12 +0100

    fix: add parentheses to nested method call

--- spec/models/role_spec.rb
+++ spec/models/role_spec.rb
@@ -7,11 +7,11 @@ RSpec.describe Role, type: :model do
 
   it 'should return true if the user has a specified role' do
     @user.add_role :admin
-    expect(@user.has_role? :admin).to be true
+    expect(@user.has_role?(:admin)).to be true
   end
 
   it 'should return false if the user does not have a specified role' do
     @user.add_role :user
-    expect(@user.has_role? :admin).to be false
+    expect(@user.has_role?(:admin)).to be false
   end
 end
