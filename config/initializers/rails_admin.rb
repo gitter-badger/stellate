aa7b22666ba88ed5544db8e2d415afad80d37761
pixeldesu <git@pixelde.su>
2016-02-12 21:18:55 +0100

    feat: configure RailsAdmin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,27 +1,14 @@
 RailsAdmin.config do |config|
 
-  ### Popular gems integration
+  config.main_app_name = ["Stellate", "Administration"]
 
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == Cancan ==
-  # config.authorize_with :cancan
-
-  ## == Pundit ==
-  # config.authorize_with :pundit
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+  config.authorize_with do
+    redirect_to main_app.root_path unless current_user.has_role? :admin
+  end
 
   config.actions do
-    dashboard                     # mandatory
-    index                         # mandatory
+    dashboard                     
+    index                         
     new
     export
     bulk_delete
@@ -29,9 +16,11 @@ RailsAdmin.config do |config|
     edit
     delete
     show_in_app
-
-    ## With an audit adapter, you can add:
-    # history_index
-    # history_show
   end
+
+  config.included_models = %w[
+    User
+    Profile
+    Role
+  ]
 end
