ec665541d4bc2f18618d9b7d0a7564c7e1762de9
pixeldesu <git@pixelde.su>
2016-02-17 21:49:20 +0100

    feat: add blocking methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,12 @@ class User < ActiveRecord::Base
   has_many :passive_relationships, class_name: 'Relationship',
                                    foreign_key: 'target_id',
                                    dependent: :destroy
+  has_many :blocked_relationships, class_name: 'Relationship',
+                                  foreign_key: 'source_id',
+                                  dependent: :destroy
   has_many :friends,   through: :active_relationships, source: :target
   has_many :followers, through: :passive_relationships, source: :source
+  has_many :blocks,    through: :blocked_relationships, source: :target
 
   validates :screen_name, presence: true,
                           uniqueness: { case_sensitive: false }
@@ -44,16 +48,39 @@ class User < ActiveRecord::Base
   end
 
   def create_relationship(target_user)
-    active_relationships.create target: target_user
+    return if target_user.blocked? self
+    active_relationships.create(target: target_user, type: "follow")
   end
   alias follow create_relationship
 
   def destroy_relationship(target_user)
-    active_relationships.find_by(target: target_user).destroy
+    active_relationships.find_by(target: target_user, type: "follow").destroy
   end
   alias unfollow destroy_relationship
 
   def following?(target_user)
-    friends.include? target_user
+    active_relationships.where(source: self, target: target_user, type: "follow").exists?
+  end
+
+  def block_relationship(target_user)
+    if self.following? target_user
+      self.unfollow target_user
+    end
+
+    if target_user.following? self
+      target_user.unfollow self
+    end
+
+    blocked_relationships.create(target: target_user, type: "block")
+  end
+  alias block block_relationship
+
+  def unblock_relationship(target_user)
+    blocked_relationships.find_by(target: target_user, type: "block").destroy
+  end
+  alias unblock unblock_relationship
+
+  def blocked?(target_user)
+    blocked_relationships.where(source: self, target: target_user, type: "block").exists?
   end
 end
