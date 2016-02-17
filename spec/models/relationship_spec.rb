a5d900a058abf32d8f74c0534cb7ef09044eea6c
pixeldesu <git@pixelde.su>
2016-02-17 21:53:30 +0100

    test: add relationship model tests for blocking

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -21,4 +21,25 @@ RSpec.describe Relationship, type: :model do
     @user.follow @user2
     expect{ @user.follow @user2 }.to raise_error(ActiveRecord::RecordNotUnique)
   end
+
+  it "should destroy all relationships after blocking" do
+    @user.follow @user2
+    @user2.follow @user
+    @user.block @user2
+    expect(@user.following?(@user2)).to be false
+    expect(@user2.following?(@user)).to be false
+  end
+
+  it "should not create a relationship if the user is blocked" do
+    @user.block @user2
+    @user2.follow @user
+    expect(@user2.following?(@user)).to be false
+  end
+
+  it "should be able to create relationships if the user is unblocked" do
+    @user.block @user2
+    @user.unblock @user2
+    @user2.follow @user
+    expect(@user2.following?(@user)).to be true
+  end
 end
