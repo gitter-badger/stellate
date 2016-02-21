51060b44138d828e054c4260d02c742cb5a7b4ba
pixeldesu <git@pixelde.su>
2016-02-21 14:40:38 +0100

    feat: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,24 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
 
   # Routes for static pages:
   root 'static#index'
   get 'faq' => 'static#faq'
   get 'about' => 'static#about'
+
+  devise_for :users, path: 'user', skip: [:sessions, :registrations]
+  as :user do
+    # :sessions
+    get '/login' => 'devise/sessions#new', as: :new_user_session
+    post '/login' => 'devise/sessions#create', as: :user_session
+    delete '/logout' => 'devise/sessions#destroy', as: :destroy_user_session
+    # :registrations
+    get 'settings/delete_account' => 'devise/registrations#cancel', as: :cancel_user_registration
+    post '/user/create' => 'devise/registrations#create', as: :user_registration
+    get '/register' => 'devise/registrations#new', as: :new_user_registration
+    get '/settings/account' => 'devise/registrations#edit', as: :edit_user_registration
+    patch '/settings/account' => 'devise/registrations#update', as: :update_user_registration
+    put '/settings/account' => 'devise/registrations#update'
+    delete '/settings/account' => 'devise/registrations#destroy'
+  end
 end
