ad6cdd575d1bcd8198573961d1d6a29f9fc30212
Andreas Nedbal <andreas.nedbal@in2code.de>
2016-03-05 14:40:28 +0100

    test: improve and fix application helper tests

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -31,21 +31,31 @@ RSpec.describe ApplicationHelper, type: :helper do
 
   describe '#list_group_item' do
     before :each do
-      @item = helper.nav_entry('test', 'http://google.com')
+      @item = helper.list_group_item('test', 'http://google.com')
+      @count = 1
+      @count2 = 0
     end
 
     it 'should return a content tag' do
-      expect(@item).to include('<li')
+      expect(@item).to include('<a')
       expect(@item).to include('http://google.com')
       expect(@item).to include('test')
     end
 
     it 'should return a badge if specified' do
-      @item2 = helper.nav_entry('test', 'http://google.com', {
-        badge: true,
+      @item = helper.list_group_item('test', 'http://google.com', {
+        badge: @count,
         badge_color: 'rainbow'
       })
-      expect(@item2).to include('badge-rainbow')
+      expect(@item).to include('badge-rainbow')
+    end
+
+    it 'should not return a badge if the count is 0' do
+      @item = helper.list_group_item('test', 'http://google.com', {
+        badge: @count2,
+        badge_color: 'rainbow'
+      })
+      expect(@item).to_not include('badge-rainbow')
     end
   end
 
