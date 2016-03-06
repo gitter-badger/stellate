29b068577d994d01148fb4211777439d56a32a0f
pixeldesu <git@pixelde.su>
2016-03-06 22:36:03 +0100

    feat: secure API with Doorkeeper

--- app/api/v1/mount.rb
+++ app/api/v1/mount.rb
@@ -1,6 +1,13 @@
+require 'doorkeeper/grape/helpers'
+
 module V1
   class Mount < API
+    helpers Doorkeeper::Grape::Helpers
     version 'v1'
+
+    before do
+      doorkeeper_authorize!
+    end
 
     mount UserAPI
   end
