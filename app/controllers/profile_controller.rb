b7bf89c563d82c81a6670db8a4fae2e7149be95a
pixeldesu <git@pixelde.su>
2016-03-08 21:12:20 +0100

    feat: implement profile settings

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,2 +1,20 @@
 class ProfileController < ApplicationController
+  before_filter :authenticate_user!
+
+  def edit
+  end
+
+  def update
+    profile_attributes = params.require(:profile).permit(:display_name,
+                                                         :bio,
+                                                         :location,
+                                                         :website)
+
+    if current_user.profile.update_attributes(profile_attributes)
+      flash[:success] = "Your profile has been updated!"
+    else
+      flash[:error] = "An error occured while saving your profile!"
+    end
+    redirect_to edit_profile_path
+  end
 end
